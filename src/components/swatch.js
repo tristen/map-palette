caed8bb60cdf035c63267b52dece53e598ee19ab
tristen <tristen.brown@gmail.com>
2016-09-23 18:44:14 -0400

    Bounds logic

--- src/components/swatch.js
+++ src/components/swatch.js
@@ -19,33 +19,31 @@ const swatchTarget = {
     const hoverIndex = props.index;
 
     // Don't replace items with themselves
-    if (dragIndex === hoverIndex) {
-      return;
-    }
+    if (dragIndex === hoverIndex) return;
 
     // Determine rectangle on screen
     const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
 
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+    // Get horizontal middle
+    const hoverMiddleX = hoverBoundingRect.left + (hoverBoundingRect.width / 2);
 
     // Determine mouse position
     const clientOffset = monitor.getClientOffset();
 
     // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+    const hoverClientX = clientOffset.x - hoverBoundingRect.left;
 
     // Only perform the move when the mouse has crossed half of the items height
     // When dragging downwards, only move when the cursor is below 50%
     // When dragging upwards, only move when the cursor is above 50%
 
     // Dragging downwards
-    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+    if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
       return;
     }
 
     // Dragging upwards
-    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+    if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
       return;
     }
 
